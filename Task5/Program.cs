72f97a2cd3eea73622fd842d0f99eaf678b6e9c6
Noveria <little.noveria@gmail.com>
2019-10-13 00:50:07 +0300

    Проект для 5 задачи

---
+++ Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*№5 Задать массив 10 чисел. Воспользовавшись вспомогательным массивом, 
+            элементы исходного массива циклически сдвинуть на 6 позиций вправо.*/
+
+        }
+    }
+}
